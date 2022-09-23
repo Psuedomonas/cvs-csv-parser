070ea2870463a5006e298b64aa2ebe9959421dcf
Pseudomonas <zehm0007@d.umn.edu>
2022-09-23 18:06:48 -0500

    Add files via upload

---
+++ cvs common drugs code.R
@@ -0,0 +1,36 @@
+# Input my csv file
+my_data <- read.csv("/home/putinisapussy/Documents/DRUG NAMES-CORRECTED.csv")
+
+# remove an unnecessary column from my data (column 1)
+df <- subset(my_data, select = -1)
+
+# sort the data by the generic name (alphabetical)
+df <- df[order(df$GENERIC),]
+
+# remove redundant listings (mostly applies to albudfuterols)
+udf <- unique(df)
+
+# re-numerate rows
+rownames <- (1:nrow(udf))
+
+# remove an unnecessary column from my data (column 1)
+sdf <- subset(udf, select = -1)
+
+#what am I doing here?
+data <- subset(udf, select = 1)
+
+test <- c('LIPITOR 10 MG TABLET','NORVASC 10 MG TABLET')
+numPos <- grep("[0-9]"
+#grep()
+#parse_number(test)
+
+def <- as.character(udf[,2])
+numPos <- regexpr("[[:digit:]]", )
+numUnits <- as.character(substr(def,numPos[1]-0, numPos[1]+4))
+
+regmatches(txt,regexpr("[0-9]+",txt))
+library(stringr)
+x <- str_extract(test, "[0-9] MG|[0-9] MCG|[0-9]%")
+y <- c("[0-9] MG", "[0-9] MCG", "[0-9]%")
+
+
---
+++ cvs common drugs code.md
@@ -0,0 +1,44 @@
+To Do
+
+I. The - er - Plan...
+
+1.  Make a dosage column from the Brand/Generic column(s)
+
+    a\. use a regex to find the dosage
+
+    b\. apply the regex
+
+    c\. make the new column
+
+2.  If brand names are consistent across dosages, collapse to a single row
+
+3.  Add dosage possibilities to the dosage row
+
+4.  Procure better information for description
+
+5.  pronunciation guide
+
+Additional stuff
+
+-   drug interactions
+
+-   Sounds like
+
+II\. Plan to implement the plan...
+
+custom regex probably necessary here
+
+conditions
+
+1.  NOT preceded by '#'
+2.  may have 'MCG', 'MG', 'GM', '%'
+
+regexpr("[[:digit:]]+ MCG" \| "[[:digit:]]+ MG" \| "[[:digit:]]+ GM \| [[:digit:]]+%", x)
+
+    > a <- list(1:3, 4:5, 6:9)
+    > b <- c(2, 3, 5, 8)
+    > g <- rep(seq_along(a), sapply(a, length))
+    > g[match(b, unlist(a))]
+    [1] 1 1 2 3
+
+int vs char matching
